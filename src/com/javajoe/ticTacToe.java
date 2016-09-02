42a6d80d1d83939d8320584879493b2458ac9975
Sean Dennis <denn0134@gmail.com>
2016-09-02 16:26:37 -0500

    Intitial commit for the CellState, VictoryState, and TicTacToe.

---
+++ src/com/javajoe/ticTacToe.java
@@ -0,0 +1,30 @@
+public class TicTacToe{
+	// NOTE: I have no illusions as to all of thei being correct.
+	//1
+	public VictoryState checkForVictor(){
+	// tells who won, and if the game has been won.
+	}
+	//2
+	public void setCell(int iCol, int iRow){
+	//takes imput from the user and passes it to another method.
+	}
+	//3
+	public CellState getCell(int iCol, int iRow){
+	//gets the values of cells for usage in other methods.
+	}
+	//4
+	public void resetGame (){
+	//returns game to initial state state.
+	}
+	//5
+	public void endTurn (){
+	//Ends the turn. Somehow.
+	}
+// ToDo: VictoryState class and CellState
+	//  CellState has 3 pices of data, all public. 3 connstants, X,O,[BLANK]
+	//Victory condition has 4: X won, O won, DRAW, and ONGOING
+	// Method on this one for ending turns
+
+
+}
+
