fdd062fe438f24e804fe08fd690a8bc048c1120c
Sean Dennis <denn0134@gmail.com>
2016-09-09 17:40:05 -0500

    SPD: Added the CellState enum.  Joe: Implemented the setCell, getCell and resetGame methods, added the initializeCellArray method and the Constructor.

--- src/com/javajoe/ticTacToe.java
+++ src/com/javajoe/ticTacToe.java
@@ -1,27 +1,83 @@
 public class TicTacToe{
+
+	public CellState[][] cellArray = new CellState[3][3];
+
+	public enum CellState{
+		X,
+		O,
+		BLANK
+	}//end CellState
+
+	public TicTacToe(){
+		initializeCellArray();
+	}// end constructer
+
+	private int cellStateToInt(CellState value){
+		switch(value){
+			case X:
+				return 1;
+
+			case O:
+				return -1;
+
+			case BLANK:
+				return 0;
+			default:
+				return -666;
+		}//end switch
+	}//end cellStateToInt
+
+	private CellState intToCellState(int iInput){
+		if (iInput == 1)
+			return CellState.X;
+		else if (iInput == -1)
+			return CellState.O;
+		else if (iInput == 0)
+			return CellState.BLANK;
+		else
+			return null;
+
+
+
+	}// End of intToCellState
+
 	// NOTE: I have no illusions as to all of thei being correct.
 	//1
-	public VictoryState checkForVictor(){
+	//public VictoryState checkForVictor(){
 	// tells who won, and if the game has been won.
-		return null;
-	}
+	//	return null;
+	//}
+
 	//2
-	public void setCell(int iCol, int iRow){
-	//takes imput from the user and passes it to another method.
+	public void setCell (int iCol, int iRow, CellState newState){
+		cellArray[iCol][iRow] = newState;
 	}
+
 	//3
 	public CellState getCell(int iCol, int iRow){
-	//gets the values of cells for usage in other methods.
-		return null;
+
+		return cellArray[iCol][iRow];
 	}
+
 	//4
 	public void resetGame (){
 	//returns game to initial state state.
 	}
+
 	//5
 	public void endTurn (){
 	//Ends the turn. Somehow.
 	}
+	private void initializeCellArray(){
+		for(int iCol = 0; iCol < 3; iCol++){
+			for(int iRow = 0; iRow < 3; iRow++){
+				cellArray[iCol][iRow] = CellState.BLANK;
+
+			}
+		}
+
+	}
+
 // ToDo: VictoryState class and CellState
 	//  CellState has 3 pices of data, all public. 3 connstants, X,O,[BLANK]
 	//Victory condition has 4: X won, O won, DRAW, and ONGOING
