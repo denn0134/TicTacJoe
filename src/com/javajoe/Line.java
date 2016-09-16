329194dad00ad039a73150af3378d2122239ed32
JaroschJr <JaroschJr@gmail.com>
2016-09-16 16:27:35 -0500

    Also just a test

--- src/com/javajoe/Line.java
+++ src/com/javajoe/Line.java
@@ -3,6 +3,7 @@ import java.awt.*;
 
 public class Line{
 	private Point cell1, cell2, cell3;
+	//constructor
 	public Line(int x1, int y1, int x2, int y2, int x3, int y3){
 		cell1 = new Point(x1, y1);
 		cell2 = new Point(x2, y2);
