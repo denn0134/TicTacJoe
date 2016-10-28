af07b5ea3f88a02c3c5fcff0c25ad701a0304bab
Sean Dennis <denn0134@gmail.com>
2016-10-28 14:43:05 -0500

    So it can throw exceptions.

---
+++ src/TicTacEngine/GameAlreadyOverException.java
@@ -0,0 +1,10 @@
+package TicTacEngine;
+
+/**
+ * Created by spdennis on 10/28/2016.
+ */
+public class GameAlreadyOverException extends TicTacExeption {
+    public GameAlreadyOverException(String message) {
+        super(message);
+    }
+}
