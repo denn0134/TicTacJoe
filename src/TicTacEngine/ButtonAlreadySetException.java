b5ccbba8a2dbd0fc170633c8afc937108fdaea8b
Sean Dennis <denn0134@gmail.com>
2016-10-28 13:41:42 -0500

    created exception classes for java.

---
+++ src/TicTacEngine/ButtonAlreadySetException.java
@@ -0,0 +1,10 @@
+package TicTacEngine;
+
+/**
+ * Created by spdennis on 10/21/2016.
+ */
+public class ButtonAlreadySetException extends TicTacExeption {
+    public ButtonAlreadySetException(String message){
+        super(message);
+    }
+}
---
+++ src/TicTacEngine/TicTacExeption.java
@@ -0,0 +1,10 @@
+package TicTacEngine;
+
+/**
+ * Created by spdennis on 10/21/2016.
+ */
+public class TicTacExeption extends Exception {
+    public TicTacExeption(String message){
+        super(message);
+    }
+}
